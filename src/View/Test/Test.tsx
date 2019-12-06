bec76dac76f966f680ef44ca0f630599e786005d
yxwyxw3038 <mywowgood@163.com>
2019-12-06 09:40:11 +0800

    改下Test

--- src/View/Test/Test.tsx
+++ src/View/Test/Test.tsx
@@ -1,25 +1,17 @@
-import React, { useState, useContext, useEffect } from "react";
-import { MyContext } from "../../Store/HookStore";
+import React, { useState, useEffect } from "react";
 import { Input } from "antd";
-import { IUserInfo } from "../../Type/Interface/Interface";
 function Test(props: any) {
-    const [info] = useState("Test");
-    const {user_info} = useContext(MyContext);
-    const [storeInfo, setStoreInfo] = useState(user_info.Data.GetInfo);
+    const [info,Setinfo] = useState("Test");
     useEffect(() => {
-        const info1:string = JSON.stringify(storeInfo);
-        window.localStorage.setItem('Test',info1);
-        },[storeInfo]);
+        window.localStorage.setItem('Test',info);
+        },[info]);
     const onTextChange= (e :any) => {
-            const AccountName:string=e.target.value
-            const temp:IUserInfo={...user_info.Data.GetInfo,AccountName} 
-            user_info.Data.Update(temp)
-            setStoreInfo(user_info.Data.GetInfo)
+            const myinfo:string=e.target.value
+            Setinfo(myinfo)
     }
     return (<div>
         <div>{info}</div>
-        <div>{user_info.Data.GetInfo.AccountName}</div>
-        <Input placeholder="请随意输入！"  value={user_info.Data.GetInfo.AccountName} onChange={onTextChange} />  
+        <Input placeholder="请随意输入！"  value={info} onChange={onTextChange} />  
         </div>
         )
 }
